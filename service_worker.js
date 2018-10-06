c72ebd13501c8ac6e6bacf9add0e9b116573dd1e
CrustyBarnacle <CrustyBarnaclee@users.noreply.github.com>
2018-10-05 22:24:14 -0700

    Updates service_worker.js, chmod a-x on *.jpg files.

--- service_worker.js
+++ service_worker.js
@@ -4,18 +4,47 @@
 
 console.log("service Worker: Registered");
 
-self.addEventListener('install', function(e) {
-    e.waitUntil(
+// Array of files to cache
+const cachedFiles = [
+    '/',
+    'index.html',
+    'restaurant.html',
+    'css/styles.css',
+    'js/dbhelper.js',
+    'js/main.js',
+    'js/restaurant_info.js',
+    'data/restaurants.json',
+    'img/1.jpg',
+    'img/2.jpg',
+    'img/3.jpg',
+    'img/4.jpg',
+    'img/5.jpg',
+    'img/6.jpg',
+    'img/7.jpg',
+    'img/8.jpg',
+    'img/9.jpg',
+    'img/10.jpg'
+];
+
+
+self.addEventListener('install', function(event) {
+    event.waitUntil(
         caches.open('v1').then(function(cache) {
             return cache.addAll(cachedFiles);
         })
     );
 });
 
-// Array of files to cache
-const cachedFiles = [
-    'index.html',
-    'restaurant.html',
-    'css/styles.css',
-    'js/dbhelper.js'
-];
+
+// Listen for fetch e - is request cached?
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            if (response) {
+                console.log('Match...');
+                return response;
+            return fetch(event.request);
+            }
+        })
+    );
+});
